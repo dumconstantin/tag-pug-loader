857c8f9a3be7f6c3472b068a818ddedc8f558198
Rafael <rsilraf@gmail.com>
2017-04-30 18:03:25 -0300

    query options handling addes. e.g.: "pug-tag?basedir=/my/dir"

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var riot = require('riot')
 var pug = require('pug')
+var loaderUtils = require("loader-utils")
 
 module.exports = function(source) {
-  source = pug.render(source, { pretty: true })
+  var options = { pretty: true },
+      config = {},
+      query = loaderUtils.parseQuery(this.query)
+  ;
+  Object.keys(query).forEach(function(attr) {
+  	config[attr] = query[attr];
+  });
+  options = Object.assign(options, config)
+
+  source = pug.render(source, options)
   return 'var riot = require("riot");\n\n' + riot.compile(source)
 };
+
